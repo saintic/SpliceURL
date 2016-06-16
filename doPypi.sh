c6b0dbcf9e63117b0a92f875c89c97330c6c837c
Mr.tao <staugur@saintic.com>
2016-06-16 13:58:51 +0800

    git automation add and push

--- doPypi.sh
+++ doPypi.sh
@@ -2,7 +2,7 @@
 #编译打包程序并上传到Pypi
 
 if [ "$1" = "upload" ]; then
-    python setup.py register sdist upload
+    python setup.py register sdist bdist_egg upload
 else
     python setup.py build
     sudo python setup.py install
